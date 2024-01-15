09a3ecd235155539e195d86045e95201f8bfe86f
lorna <lornah.gitau@student.moringaschool.com>
2024-01-15 10:37:54 +0300

    completed assignments

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -9,7 +7,7 @@ from models import db, Newsletter
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True  # Enable pretty-printing of JSON responses
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,7 +15,44 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+        response = make_response(response_dict, 200)
+        return response
+
+api.add_resource(Home, '/')
+
+class Newsletters(Resource):
+    def get(self):
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        response = make_response(response_dict_list, 200)
+        return response
+
+    def post(self):
+        new_record = Newsletter(
+            title=request.json.get('title'),
+            body=request.json.get('body'),
+        )
+        db.session.add(new_record)
+        db.session.commit()
+        response_dict = new_record.to_dict()
+        response = make_response(response_dict, 201)
+        return response
+
+api.add_resource(Newsletters, '/newsletters')
+
+class NewsletterByID(Resource):
+    def get(self, id):
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        if newsletter:
+            response_dict = newsletter.to_dict()
+            return make_response(response_dict, 200)
+        else:
+            return make_response({"error": "Newsletter not found"}, 404)
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
